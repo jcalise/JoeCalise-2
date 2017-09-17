82937bd21646ba961d7da0c74ac8bf247841dddd
Joe <joe.calise@gmail.com>
2017-09-17 15:04:34 -0700

    Counter assignment

---
+++ Flask/Counter/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, session, redirect
+
+app = Flask(__name__)
+app.secret_key = "SecretySecretSeccccccret"
+
+
+@app.route('/')
+def index():
+    try:
+        session['count'] += 1
+    except:
+        session['count'] = 1
+
+    return render_template("index.html")
+
+@app.route('/plus2')
+def add2():
+    session['count'] += 1
+    return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session['count'] = 0
+    return redirect('/')
+
+app.run(debug=True)
