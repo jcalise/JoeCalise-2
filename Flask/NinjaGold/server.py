b5c8afe2cddacfda2f16a6f5448bb9479f4a3f83
Joe <joe.calise@gmail.com>
2017-09-17 18:08:27 -0700

    Updating Ninja Gold game with working activity stream

--- Flask/NinjaGold/server.py
+++ Flask/NinjaGold/server.py
@@ -13,23 +13,38 @@ def index():
 @app.route('/process_money', methods=['POST'])
 def process_money():
     building = request.form['building']
-
-    if (building == "farm"):
-        g = random.randrange(10,20)
-        print("Going to the farm", g)
-    elif (building == "cave"):
-        print("Going to the cave!")
-    elif (building == "house"):
-        print("Going to the house!")
-    elif (building == "casino"):
-        print("Going to the casino!")
-
     if 'activities' not in session:
         session['activities'] = []
+    activities = session['activities']
 
-    session['activities'].append("You went to the " + building)
+    if (building == "farm"):
+        g = random.randrange(10,21)
+        session['gold'] += g
+        activities.append("You went to the " + building + " and earned " + str(g))
+    elif (building == "cave"):
+        g = random.randrange(5,11)
+        session['gold'] += g
+        activities.append("You went to the " + building + " and earned " + str(g))
+    elif (building == "house"):
+        g = random.randrange(2,6)
+        session['gold'] += g
+        activities.append("You went to the " + building + " and earned " + str(g))
+    elif (building == "casino"):
+        g = random.randrange(0,50)
+        win = random.choice([True, False])
+        if win:
+            print("You won")
+            session['gold'] += g
+            activities.append("You went to the " + building + " and won " + str(g))
+        else:
+            print("You lost")
+            session['gold'] -= g
+            activities.append("You went to the " + building + " and lost " + str(g) + "! Ouch. :(")
 
-    print(session['activities'])
+
+
+    session['activities'] = activities
+
     return redirect('/')
 
 @app.route('/clearall')
